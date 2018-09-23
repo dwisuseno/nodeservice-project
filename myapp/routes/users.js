bcfe663c6807cca5b77143fd8e3e774f2c286cdf
dicky irwanto <dicky.irwanto.its@gmail.com>
2018-09-23 22:54:13 +0700

    add mysql connection in /users

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+//mysql
+var mysql = require("mysql");
+
+// database connection
+global.connection = mysql.createConnection({
+		host : 'localhost',
+		user : 'root',
+		password: '',
+		port: '3306',
+		database: 'db_dsis',
+		connectionLimit : 10,
+		multipleStatements : true
+});
+connection.connect();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	global.connection.query('SELECT * from dsis_hse_simper', function (error, results, fields) {
+		if (error) {
+			console.log('aw');
+			throw error;
+		}
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+  
 });
 
 module.exports = router;
